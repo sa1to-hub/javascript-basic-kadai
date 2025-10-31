104ec420461b6a972098c06bf393d85a2675c2af
sa1to-hub <saisai31to@gmail.com>
2025-10-31 19:36:40 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,13 @@
+let num = 15;
+
+switch (true) {
+  case num % 15 === 0:
+    console.log("3と5の倍数です");
+    break;
+  case num % 3 === 0:
+    console.log("3の倍数です");
+    break;
+  case num % 5 === 0:
+    console.log("5の倍数です");
+    break;
+}
